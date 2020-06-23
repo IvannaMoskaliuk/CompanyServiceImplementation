01e04e9473b8861be2adaac17c1219875438f35a
Ivanna <ivanka_mos@ukr.net>
2020-06-23 20:30:16 +0300

    Initial commit

---
+++ src/com/company/ICompanyServiceImpl.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import com.company.COMPANY.Company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ICompanyServiceImpl implements ICompanyService {
+    /**
+     * @param child - company for which we are searching the top level parent
+     *              (parent of parent of ...)
+     * @return top level paren
+     */
+    @Override
+    public Company getTopLevelParent(Company child) {
+        if (child.getParent() == null){
+            return child;
+        }
+        Company company = child;
+        while (company.getParent() != null){
+            company = company.getParent();
+        }
+        return company;
+    }
+
+    /**
+     * @param company   - company for which we are searching the count of employees
+     *                  (count of this company employees +
+     *                  count employees of all children and their children employees )
+     * @param companies - list of all available companies
+     * @return count of employees
+     */
+    @Override
+    public long getEmployeeCountForCompanyAndChildren(Company company, List<Company> companies) {
+
+        companies.add(company);
+
+
+
+       long employees = 0;
+
+ /*      if (company.getEmployeesCount() != 0){
+           return employees + company.getEmployeesCount();
+       }
+*/
+
+       employees = companies.stream().mapToLong(Company::getEmployeesCount).sum();
+
+
+
+        return employees;
+
+    }
+}
